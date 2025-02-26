f6f03adddd9e31b67ada0158687e8a0f14dd76cc
Kelly Hutchins <khutchins@esri.com>
2025-02-26 14:04:38 -0800

    feat: add demo app that uses describedby and live regions

---
+++ demos/description-region/README.md
@@ -0,0 +1,9 @@
+# Esri Developer & Technology Summit Template
+
+### Project description
+
+This application demonstrates how to set an `aria-describedby` attribute for the map element to improve accessibility. It also showcases the use of `aria-live` regions to provide dynamic updates to assistive technologies.
+
+For more information on how to use `aria-describedby`, refer to the [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-describedby).
+
+To learn more about `aria-live` and its usage, visit the [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions).
---
+++ demos/description-region/app.js
@@ -0,0 +1,33 @@
+const toggleModeEl = document.getElementById("toggle-mode");
+const mapEl = document.querySelector("arcgis-map");
+const darkModeCss = document.getElementById("jsapi-mode-dark");
+const lightModeCss = document.getElementById("jsapi-mode-light");
+const liveRegion = document.getElementById("liveRegion");
+
+let mode = "light";
+
+toggleModeEl.addEventListener("click", handleModeChange);
+mapEl.addEventListener("arcgisViewReadyChange", handleArcgisViewReadyChange);
+
+function handleArcgisViewReadyChange(event) {
+  const mapDescription = document.getElementById("map-description");
+  const { portalItem } = event.target.map;
+  liveRegion.innerText = `${portalItem.title} map has loaded.`;
+  mapDescription.innerText = portalItem.snippet;
+  document.querySelectorAll(".esri-view-surface").forEach(el =>
+    el.setAttribute("aria-describedby", "map-description")
+  );
+}
+
+function handleModeChange() {
+  mode = mode === "dark" ? "light" : "dark";
+  const isDarkMode = mode === "dark";
+  darkModeCss.disabled = !isDarkMode;
+  lightModeCss.disabled = isDarkMode;
+  toggleModeEl.icon = isDarkMode ? "moon" : "brightness";
+  document.body.className = isDarkMode ? "calcite-mode-dark" : "";
+
+  document.querySelectorAll(`.calcite-mode-${isDarkMode ? "light" : "dark"}`).forEach(node =>
+    node.classList.replace(`calcite-mode-${isDarkMode ? "light" : "dark"}`, `calcite-mode-${mode}`)
+  );
+}
